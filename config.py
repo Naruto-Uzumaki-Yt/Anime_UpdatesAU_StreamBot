11528b4183bc00bf709968f8f9b0ca820434f846
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-05-07 10:31:04 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+BASE_URL = "https://your-app.onrender.com"
+
+CHANNEL_USERNAME = "Anime Updates"
+CHANNEL_LINK = "https://t.me/Anime_UpdatesAU"
+
+DEV_NAME = "Mohammed"
+DEV_LINK = "https://t.me/Mr_Mohammed_29"
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
