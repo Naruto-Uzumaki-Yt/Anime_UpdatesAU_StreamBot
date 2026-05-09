39974dac2b81d247930956430cdf996e385ccc2d
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-05-09 14:23:45 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
+# Don't Remove Credit
+# Ask Doubt @AU_Bot_Discussion
+# Owner @Mr_Mohammed_29
 # ------------------------- #
 
 import os
@@ -10,16 +10,44 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-BASE_URL = os.getenv("BASE_URL")
+# Your Render URL
+BASE_URL = os.getenv(
+    "BASE_URL",
+    "https://anime-updatesau-streambot.onrender.com"
+)
 
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-CHANNEL_LINK = os.getenv("CHANNEL_LINK")
+# Force Subscribe Channel
+CHANNEL_USERNAME = os.getenv(
+    "CHANNEL_USERNAME",
+    "Anime_UpdatesAU"
+)
 
-DEV_NAME = os.getenv("DEV_NAME")
-DEV_LINK = os.getenv("DEV_LINK")
+# Updates Channel Link
+CHANNEL_LINK = os.getenv(
+    "CHANNEL_LINK",
+    "https://t.me/Anime_UpdatesAU"
+)
+
+# Developer Name
+DEV_NAME = os.getenv(
+    "DEV_NAME",
+    "Mr Mohammed 29"
+)
+
+# Developer Link
+DEV_LINK = os.getenv(
+    "DEV_LINK",
+    "https://t.me/Mr_Mohammed_29"
+)
+
+# Thumbnail Banner URL
+THUMB_URL = os.getenv(
+    "THUMB_URL",
+    "https://envs.sh/Gz2.jpg"
+)
 
 # ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
+# Don't Remove Credit
+# Ask Doubt @AU_Bot_Discussion
+# Owner @Mr_Mohammed_29
 # ------------------------- #
